6f47577990d43d63d77fff21b546222f93511339
Geniussteven <101381139+Geniussteven@users.noreply.github.com>
2022-03-23 22:30:52 +0800

    Update 5.1_notes_and_practices.py

--- 5.1_notes_and_practices.py
+++ 5.1_notes_and_practices.py
@@ -19,3 +19,29 @@ for i in [5, 4, 3, 2, 1] :
     print(i)
 print('Blastoff!')
 #5.3 loop idioms
+#Assignment 5.1
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "Done" :
+        break
+    try :
+        newnum = int(num)
+     if largest is None or largest < newnum : 
+         largest = newnum 
+     elif smallest is None or newnum < smallest :
+         smallest = newnum 
+    except :
+        print('Invalid input')
+        continue
+print('Maximum is', largest)
+print('Minimum is', smallest)
+#两个错误，1. if largest is None，弄错赋值None对象。2.用or连结相同语句，两个if elif 也可以用。
+#filtering过滤 in a loop
+#Boolean is a kind of variable that either has the value True or False,
+#is 比 = =更强烈表达两者完全相等 You shouldn't use is when you should be using double equals, 
+#usually you're using them for a True, False, or None. So that we don't overuse is, because is,
+#is a really, really strong equality. So it's a stronger equality than double equals to,
+#so the double equals is mathematically equal to with potential conversion.
+#smallest = None(flag value)
