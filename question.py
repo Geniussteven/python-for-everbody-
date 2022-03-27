d97d86de8b7b67543d42a7d11b3b807adb5a10e7
Geniussteven <101381139+Geniussteven@users.noreply.github.com>
2022-03-27 19:21:32 +0800

    Add files via upload

---
+++ question.py
@@ -0,0 +1,3 @@
+#一些疑惑的点
+#Python exit commands - why so many and when should each be used?
+#https://stackoverflow.com/questions/19747371/python-exit-commands-why-so-many-and-when-should-each-be-used
