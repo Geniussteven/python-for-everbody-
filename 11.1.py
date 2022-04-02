cba3eae2d5ab48f8df6f02205c5895b65e963fa9
Geniussteven <101381139+Geniussteven@users.noreply.github.com>
2022-04-02 16:35:09 +0800

    Update 11.1.py

--- 11.1.py
+++ 11.1.py
@@ -9,3 +9,9 @@ https://www.notion.so/f3f75ef747934f1da4f9b36771fc0f49
 re.search()返回对与错根据字符串时候match正则表达式
 re.findall() 让matched string to be extracted print出字符串组
 Greedy Matching
+import re
+x = '一串含数字的字符串'
+y = re.findall('[0-9]+', x)
+greedy matching prefer longest eg ^A.+:
+Non-greedy Matching 倾向于shortest  ^A.+?:
+  
