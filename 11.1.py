89597b66b1b6c3a1adf086d9183b4502c1c7dce4
Geniussteven <101381139+Geniussteven@users.noreply.github.com>
2022-04-06 18:52:32 +0800

    Update 11.1.py

--- 11.1.py
+++ 11.1.py
@@ -14,4 +14,44 @@ x = '一串含数字的字符串'
 y = re.findall('[0-9]+', x)
 greedy matching prefer longest eg ^A.+:
 Non-greedy Matching 倾向于shortest  ^A.+?:
-  
+  Assignment 11.1
+  import re
+fname = input("Enter file:")
+handle = open(fname)
+list = ()
+y = re.findall('[0-9]+', handle)
+#此时的y是lists of strings
+for line in handle ：
+  i = float(y[0])
+  y = y[0:  ]
+  x  = sum(i)
+other method
+1st:
+import re#use the 推导式comprehensions
+print(sum([int(i) for i in (re.findall('[0-9]+',(open('filename.txt','r').read())))]))
+2nd
+import re 
+hand = open("regex_sum_24962.txt")
+x=list()
+for line in hand :
+    y = re.findall('[0-9]+', line)
+    x = x + y
+sum = 0
+for z in x :
+    sum = sum + int(z)
+print(sum)
+
+3rd
+import re 
+hand = open("regex_sum_24962.txt")
+x = []
+for line in hand :
+    num = line.rstrip()
+    numbers  = re.findall('[0-9]+', line)
+for number in numbers
+    if number != ''：
+    lst.append(int(number))
+print(sum(x))
+
+'''for num in x:
+    num_list.append(int(num))'''也可以
