6f3828568a72a551457c45a05d402fe0b7995a4e
Geniussteven <101381139+Geniussteven@users.noreply.github.com>
2022-04-13 21:13:17 +0800

    Add files via upload

---
+++ 12.3_Notes.py
@@ -0,0 +1,19 @@
+显示字母位置
+print(ord('H'))#ordinal
+72
+unicode通用sets
+ASCII
+American Standard Code for I
+UTF-8 1-4bytes The best way to encode data
+UTF-16 2
+UTF-32 4
+在python3
+x = 'hahaha'
+type(x)->class str
+x = u'hahaha'
+type(x)->class str
+在python2
+x = u'hahaha'
+type(x)->class unicode
+prefixing前缀
+ And so there's always been a thing like a byte string and they do this by prefixing the b.
