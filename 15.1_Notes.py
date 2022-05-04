de555bbfb2e8ff379c8ea775bf0df40068bbeefd
Geniussteven <101381139+Geniussteven@users.noreply.github.com>
2022-05-04 14:08:16 +0800

    Update 15.1_Notes.py

--- 15.1_Notes.py
+++ 15.1_Notes.py
@@ -1,1 +1,11 @@
 #15.1 Relational Databases
+#以连接为模型的概念是使数据库快速发展的基础数学
+'''column列， row行
+schema就是类似于表格有关标题那第一行，后面每一行必须要是整数，字符串不超过128个字符。
+首先我们会建立一个database application（DB又称为API） ，它可以理解复杂的数据，
+然后我们的code借用这个和复杂的database进行沟通。这个过程称之为abstraction抽象。
+我们用SQL(Structured Query Language)和DB沟通。'''
+SQL四个基础函数
+CRUD-create a table、retrieve some data、insert data、delete data
+
+#15.2 - Using Databases
