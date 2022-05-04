c768fef28adffc6f212315ad8ffda4334e9aa2e7
Geniussteven <101381139+Geniussteven@users.noreply.github.com>
2022-05-04 15:15:09 +0800

    Update 15.1_Notes.py

--- 15.1_Notes.py
+++ 15.1_Notes.py
@@ -9,3 +9,17 @@ SQL四个基础函数
 CRUD-create a table、retrieve some data、insert data、delete data
 
 #15.2 - Using Databases
+首先
+我们要建立表格放数据进去。做的是DBA的工作
+在SQLite browser里输入内容
+然后
+我们要编写python程序和数据库沟通
+input files 然后经过程序清理，存储到database
+然后编写另一个程序读取数据，然后output（eg.R数据分析、D3.js）
+总之，读取数据，清 理数据，并以合理和有组织的方式将其粘贴在数据库中。 然后编写其他应用程序来理解它并分析数据。
+
+#databases model
+'''数据模型不仅仅是查询它的 SQL，而是我们要在数据库中存储和检索内容的形状的合同。
+SQLite 就是所谓的嵌入式数据库。MySQL 或 Oracle 是一整套独立的软件 ，我们通过网络连接讨论。 但 SQLite 实际上是软件的一部分，所以它是python内置的.
+它的速度很快，并针对更少量的数据
+导入语句：import statement'''
