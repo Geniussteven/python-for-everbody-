baa0b0e87229ddf7b99216e77a42c40e6883abb6
Geniussteven <101381139+Geniussteven@users.noreply.github.com>
2022-07-16 17:53:19 +0800

    Create 15.8 - Many-to-Many Relationships.py

---
+++ 15.8 - Many-to-Many Relationships.py
@@ -0,0 +1,68 @@
+#通过建立中间表(junction table)来decompose many to many relationship.
+#中间表只需要有几个对应的foreign key， 还可以加上role（put a little bit of extra data on the link itself）、
+'''ORDER 后续优先级递减，然后有DESC后缀的就是降序排列。'''
+#Worked Example: roster.py (Chapter 15)
+
+import json
+import sqlite3
+
+conn = sqlite3.connect('rosterdb.sqlite')
+cur = conn.cursor()
+
+# Do some setup
+cur.executescript('''
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
+
+CREATE TABLE User (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name   TEXT UNIQUE
+);
+
+CREATE TABLE Course (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title  TEXT UNIQUE
+);
+
+CREATE TABLE Member (
+    user_id     INTEGER,
+    course_id   INTEGER,
+    role        INTEGER,
+    PRIMARY KEY (user_id, course_id)
+)
+''')
+
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = 'roster_data_sample.json'
+
+# [
+#   [ "Charley", "si110", 1 ],
+#   [ "Mea", "si110", 0 ],
+
+str_data = open(fname).read()
+json_data = json.loads(str_data)
+
+for entry in json_data:
+
+    name = entry[0]
+    title = entry[1]
+
+    print((name, title))
+
+    cur.execute('''INSERT OR IGNORE INTO User (name)
+        VALUES ( ? )''', ( name, ) )
+    cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
+    user_id = cur.fetchone()[0]#fetch one record from the cursor
+
+    cur.execute('''INSERT OR IGNORE INTO Course (title)
+        VALUES ( ? )''', ( title, ) )
+    cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
+    course_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Member
+        (user_id, course_id) VALUES ( ?, ? )''',
+        ( user_id, course_id ) )
+
+    conn.commit()
